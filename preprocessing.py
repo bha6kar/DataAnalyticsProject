3dc27929870808fbc7efbe784558d6dcf8014f6c
onyx <bhaskar.droid@gmail.com>
2019-04-26 12:24:32 +0100

    model changed

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,9 @@
+# preprocessing.py
+
 import numpy as np
 import pandas as pd
 import pywt
 import matplotlib.pyplot as plt
-
 
 class PreProcessing:
     def __init__(self, split, feature_split, stock_data=pd.read_csv("stock_data.csv")):
@@ -26,21 +27,15 @@ class PreProcessing:
                 tx = pywt.idwt(cat, cdt, "haar")
                 log = np.diff(np.log(tx))*100
                 macd = np.mean(x[5:]) - np.mean(x)
-                # ma = np.mean(x)
                 sd = np.std(x)
                 log_ret = np.append(log_ret, log)
                 x_tech = np.append(macd*10, sd)
                 train = np.append(train, x_tech)
-            train_data.append(train)
             log_train_data.append(log_ret)
-        trained = pd.DataFrame(train_data)
-        trained.to_csv("preprocessing/indicators.csv")
+
         log_train = pd.DataFrame(log_train_data, index=None)
         log_train.to_csv("preprocessing/log_train.csv")
-        # auto_train = pd.DataFrame(train_data[0:800])
-        # auto_test = pd.DataFrame(train_data[801:1000])
-        # auto_train.to_csv("auto_train.csv")
-        # auto_test.to_csv("auto_test.csv")
+
         rbm_train = pd.DataFrame(log_train_data[0:int(self.split*self.feature_split*len(log_train_data))], index=None)
         rbm_train.to_csv("preprocessing/rbm_train.csv")
         rbm_test = pd.DataFrame(log_train_data[int(self.split*self.feature_split*len(log_train_data))+1:
@@ -54,7 +49,6 @@ class PreProcessing:
 
     def make_test_data(self):
         test_stock = []
-        # stock_data_test = pd.read_csv("stock_data_test.csv", index_col=0)
 
         for i in range((len(self.stock_data) // 10) * 10 - 11):
             l = self.stock_data.iloc[i+11, 5]
@@ -66,15 +60,3 @@ class PreProcessing:
                                                self.split*(1-self.feature_split)*len(test_stock)):]
         stock = pd.DataFrame(stock_test_data, index=None)
         stock.to_csv("stock_data_test.csv")
-
-        # print(train_data[1:5])
-        # print(test_data[1:5])
-        # plt.plot(train_data[1])
-        # plt.show()
-
-
-if __name__ == "__main__":
-    preprocess = PreProcessing(0.8, 0.25)
-    preprocess.make_wavelet_train()
-    preprocess.make_test_data()
-
